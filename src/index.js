b848ec9ee67e7f1c4f300fe0b6e6eb64b35ca6f8
chinweokwu <paulcmorah@gmail.com>
2020-12-03 14:33:02 +0100

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import{DisplayHomePage}from'./home';
-import{DisplayMenuPage}from'./menu';
-import{DisplayContactPage}from'./contact';
-import{Navbar}from'./nav';
+/* eslint-disable  no-undef, no-plusplus, camelcase, no-restricted-syntax, import/no-cycle, import/prefer-default-export, no-plusplus, max-len */
+import{ DisplayHomePage }from'./home';
+import{ DisplayMenuPage }from'./menu';
+import{ DisplayContactPage }from'./contact';
+import{ Navbar }from'./nav';
 
 const header = document.querySelector('header');
 Navbar.createNav(header);
 const contentdiv = document.getElementById('content');
 DisplayHomePage.homepage(contentdiv);
 
-document.getElementById('home').addEventListener('click',()=>{
+document.getElementById('home').addEventListener('click', ()=>{
     contentdiv.innerHTML = '';
     DisplayHomePage.homepage(content);
 });
 
-document.getElementById('menu').addEventListener('click',()=>{
+document.getElementById('menu').addEventListener('click', ()=>{
     contentdiv.innerHTML = '';
     DisplayMenuPage.menupage(content);
 });
 
-document.getElementById('contact').addEventListener('click',()=>{
+document.getElementById('contact').addEventListener('click', ()=>{
     contentdiv.innerHTML = '';
     DisplayContactPage.contactpage(content);
 });
